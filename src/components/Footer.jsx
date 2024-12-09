821d58823c8794a086d8d4326ed97c4f8a0a45d0
Favored Online Skills Acquisition Network <69410084+Favoredonline@users.noreply.github.com>
2024-12-09 06:37:31 -0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
-    <footer className="w-full text-center py-8 bg-[#1a1a1a] text-white text-sm mt-16">
+    <footer className="w-full text-center py-6 bg-[#1a1a1a] text-white text-sm mt-6">
       &copy; {currentYear} All rights reserved
     </footer>
   );
