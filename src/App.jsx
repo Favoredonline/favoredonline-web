0d24bcf6e80dfc91af17d5fb70f4329f30baba3a
Favored Online Skills Acquisition Network <69410084+Favoredonline@users.noreply.github.com>
2024-12-19 10:54:55 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,11 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './Pages/Home'
 import ScrollToTop from './components/ScrollToTop'
 import Navbar from './components/Navbar'
-import About from './Pages/About'
-import About from './Pages/Events'
+import About from './Pages/About' // Keep this as About
+import Events from './Pages/Events' // Rename this to Events
 import Footer from './components/Footer'
 
 function App() {
-
   return (
     <div className='bg-[#1a1a1a] min-h-screen'>
       <BrowserRouter>
@@ -17,12 +16,11 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
-          <Route path='/event' element={<Events />} />
+          <Route path='/event' element={<Events />} /> {/* Update here */}
         </Routes>
         <Footer />
       </BrowserRouter>
     </div>
-
   )
 }
 
