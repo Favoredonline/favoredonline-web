bab825a182c4ef80fb61cce721caf674d525d121
Favored Online Skills Acquisition Network <69410084+Favoredonline@users.noreply.github.com>
2024-12-19 10:50:42 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './Pages/Home'
 import ScrollToTop from './components/ScrollToTop'
 import Navbar from './components/Navbar'
 import About from './Pages/About'
+import About from './Pages/Events'
 import Footer from './components/Footer'
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
+          <Route path='/event' element={<Events />} />
         </Routes>
         <Footer />
       </BrowserRouter>
