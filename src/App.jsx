f7233626e349a79916a9c92462aaad00dcdaf2a3
Favored Online Skills Acquisition Network <69410084+Favoredonline@users.noreply.github.com>
2024-12-19 11:01:45 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
-          <Route path='/event' element={<Events />} /> {/* Update here */}
+          <Route path='/events' element={<Events />} /> {/* Update here */}
         </Routes>
         <Footer />
       </BrowserRouter>
