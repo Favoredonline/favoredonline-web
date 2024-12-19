61bea8ad69dea4982d28fc8fc25f0cf659cb4cb8
Favored Online Skills Acquisition Network <69410084+Favoredonline@users.noreply.github.com>
2024-12-19 14:49:59 -0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,40 +1,41 @@
 import { useState } from "react";
-import { FiMenu, FiX } from "react-icons/fi"
-import logo from '../assets/logo.png';
+import { FiMenu, FiX } from "react-icons/fi";
+import logo from "../assets/logo.png";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-
-    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   const toggleMobileMenu = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen);
   };
+
   return (
     <div>
-        <nav className="relative z-20 mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center py-6">
-        <Link to={'/'} className="flex items-center">
+      <nav className="relative z-20 mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center py-6">
+        <Link to={"/"} className="flex items-center">
           <img src={logo} alt="Favored Online Inc Logo" className="w-40" />
         </Link>
         <div className="space-x-4 hidden md:block">
           <button className="text-sm bg-[#81bb36] text-white px-6 py-2">
-            <Link to={'/about'} className="font-medium text-white uppercase text-sm">About US</Link>
-          </button>  
-            <button className="text-sm bg-[#007BFF] text-white px-6 py-2">
-            <Link to={'/events'} className="font-medium text-white uppercase text-sm">Event</Link>
-          </button>  
-            <button className="text-sm bg-[#007BFF] text-white px-6 py-2">
-            <Link to={'/summit'} className="font-medium text-white uppercase text-sm">Summit</Link>
-          </button>  
-            </button>  
-            <button className="text-sm bg-[#007BFF] text-white px-6 py-2">
-            <Link to={'/conference'} className="font-medium text-white uppercase text-sm">Conference</Link>
-          </button>  
-          <button className="text-sm bg-[#EB48D5] text-white px-4 py-2">
-            <a href="https://www.shemodeltech.com" target="_blank">SHE MODEL TECH</a>
+            <Link to={"/about"} className="font-medium text-white uppercase text-sm">
+              About Us
+            </Link>
           </button>
-          <button className="text-sm bg-[#D2AE2F] text-white px-4 py-2">
-            <a href="https://www.techtalentscity.com" target="_blank">TECHTALENTS CITY</a>
+          <button className="text-sm bg-[#007BFF] text-white px-6 py-2">
+            <Link to={"/events"} className="font-medium text-white uppercase text-sm">
+              Events
+            </Link>
+          </button>
+          <button className="text-sm bg-[#EB48D5] text-white px-6 py-2">
+            <Link to={"/summit"} className="font-medium text-white uppercase text-sm">
+              Summit
+            </Link>
+          </button>
+          <button className="text-sm bg-[#D2AE2F] text-white px-6 py-2">
+            <Link to={"/conference"} className="font-medium text-white uppercase text-sm">
+              Conference
+            </Link>
           </button>
         </div>
         <div className="md:hidden z-40">
@@ -51,28 +52,30 @@ const Navbar = () => {
               <FiX />
             </button>
           </div>
-          <button className="text-sm bg-[#81bb36] text-white px-10 py-2  mb-4">
-          <Link to={'/about'} className="font-medium text-white uppercase" onClick={toggleMobileMenu}>About US</Link>
+          <button className="text-sm bg-[#81bb36] text-white px-10 py-2 mb-4">
+            <Link to={"/about"} className="font-medium text-white uppercase" onClick={toggleMobileMenu}>
+              About Us
+            </Link>
           </button>
-          <button className="text-sm bg-[#007BFF] text-white px-10 py-2  mb-4">
-          <Link to={'/events'} className="font-medium text-white uppercase" onClick={toggleMobileMenu}>Event</Link>
+          <button className="text-sm bg-[#007BFF] text-white px-10 py-2 mb-4">
+            <Link to={"/events"} className="font-medium text-white uppercase" onClick={toggleMobileMenu}>
+              Events
+            </Link>
           </button>
-            <button className="text-sm bg-[#007BFF] text-white px-10 py-2  mb-4">
-          <Link to={'/summit'} className="font-medium text-white uppercase" onClick={toggleMobileMenu}>Summit</Link>
+          <button className="text-sm bg-[#EB48D5] text-white px-10 py-2 mb-4">
+            <Link to={"/summit"} className="font-medium text-white uppercase" onClick={toggleMobileMenu}>
+              Summit
+            </Link>
           </button>
-            <button className="text-sm bg-[#007BFF] text-white px-10 py-2  mb-4">
-          <Link to={'/conference'} className="font-medium text-white uppercase" onClick={toggleMobileMenu}>Conference</Link>
-          </button>
-          <button className="text-sm bg-[#EB48D5] text-white px-4 py-2 mb-4">
-            <a href="https://www.shemodeltech.com" target="_blank">SHE MODEL TECH</a>
-          </button>
-          <button className="text-sm bg-[#D2AE2F] text-white px-4 py-2">
-            <a href="https://www.techtalentscity.com" target="_blank">TECHTALENTS CITY</a>
+          <button className="text-sm bg-[#D2AE2F] text-white px-10 py-2 mb-4">
+            <Link to={"/conference"} className="font-medium text-white uppercase" onClick={toggleMobileMenu}>
+              Conference
+            </Link>
           </button>
         </div>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
