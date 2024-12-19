a193a83bd6b30b8b01274b404238059f82c364d6
Favored Online Skills Acquisition Network <69410084+Favoredonline@users.noreply.github.com>
2024-12-19 10:23:23 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './Pages/Home'
 import ScrollToTop from './components/ScrollToTop'
 import Navbar from './components/Navbar'
 import About from './Pages/About'
+import About from './Pages/Events'
 import Footer from './components/Footer'
 
 function App() {
