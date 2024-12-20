548ebaac9f5005f26084a1b349bbfef06b39d8f9
Favored Online Skills Acquisition Network <69410084+Favoredonline@users.noreply.github.com>
2024-12-20 14:46:51 -0500

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -29,36 +29,36 @@ const Hero = () => {
       style={{ backgroundImage: `url(${backg})` }}
     >
       {/* Navigation Bar */}
-       <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-         <div className="text-center pt-16 pb-12 md:pb-20">
-           <h1 className="font-extrabold tracking-tight text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-transparent bg-clip-text bg-gradient-to-r from-[#EB48D5] to-[#D2AE2F] leading-snug">
-           Leading the Future of {' '} <br />
-            <span className="block">Tech through Education</span>
+        <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="text-center pt-16 pb-12 md:pb-20">
+          <h1 className="font-bold tracking-tight text-2xl sm:text-3xl md:text-5xl lg:text-6xl xl:text-7xl">
+            LEADING THE FUTURE OF {' '} <br />
+            <span className="text-[#EB48D5]">TECH</span> THROUGH{' '}
+            <span className="text-[#D2AE2F]">EDUCATION</span>
           </h1>
         </div>
-       </div>
+      </div>
       <div className="relative z-10 flex flex-col lg:flex-row justify-center items-center md:mt-8 pb-8 md:pb-16 2xl:pb-24 px-12 xl:px-20">
-        <div className="relative w-[350px] md:w-[600px] lg:w-[890px] h-[280px] md:h-[350px] xl:h-[500px] overflow-hidden rounded-xl shadow-lg">
+        <div className="relative w-[350px] md:w-[600px] lg:w-[890px] h-[280px] md:h-[350px] xl:h-[500px]">
           <img
             src={ttc}
             alt="Tech Talent City"
-            className="absolute w-full h-full object-cover z-0 transform hover:scale-105 transition-transform duration-500 ease-in-out"
+            className="absolute w-full h-full object-cover z-0"
           />
-          <div className="absolute inset-0 flex flex-col justify-end items-center p-8 z-10 bg-gradient-to-t from-black/70 to-transparent rounded-xl">
-            <p className="max-w-xs mt-3 text-center text-white text-lg font-medium">
+          <div className="absolute inset-0 flex flex-col justify-end items-center p-8 z-10 bg-black/30">
+            <p className="max-w-xs mt-3 text-center">
               Inspiring and Equipping the Next Generation of Female Tech Leaders
             </p>
           </div>
         </div>
-
-        <div className="relative w-[350px] md:w-[600px] lg:w-[890px] h-[280px] md:h-[350px] xl:h-[500px] mt-8 lg:mt-0 lg:ml-8 overflow-hidden rounded-xl shadow-lg">
+        <div className="relative w-[350px] md:w-[600px] lg:w-[890px] h-[280px] md:h-[350px] xl:h-[500px]">
           <img
             src={smt}
             alt="She Model Tech"
-            className="absolute inset-0 w-full h-full object-cover z-0 transform hover:scale-105 transition-transform duration-500 ease-in-out"
+            className="absolute inset-0 w-full h-full object-cover z-0"
           />
-          <div className="absolute inset-0 flex flex-col justify-end items-center p-8 z-10 bg-gradient-to-t from-black/70 to-transparent rounded-xl">
-            <p className="max-w-xs mt-3 text-center text-white text-lg font-medium">
+          <div className="absolute inset-0 flex flex-col justify-end items-center p-8 z-10 bg-black/30">
+            <p className="max-w-xs mt-3 text-center">
               Cultivating a Thriving Ecosystem for Emerging Tech Professionals
             </p>
           </div>
