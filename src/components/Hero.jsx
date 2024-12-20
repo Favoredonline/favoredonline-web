9fa95c9e78729b1d2a0bf3b292bcbbfd5624d930
Favored Online Skills Acquisition Network <69410084+Favoredonline@users.noreply.github.com>
2024-12-20 14:35:41 -0500

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,15 +28,15 @@ const Hero = () => {
       className="relative bg-black/20 text-white h-full bg-center bg-cover"
       style={{ backgroundImage: `url(${backg})` }}
     >
-            {/* Navigation Bar */}
-      <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="text-center pt-16 pb-12 md:pb-20">
-          <h1 className="font-extrabold tracking-tight text-3xl sm:text-4xl md:text-6xl lg:text-7xl xl:text-8xl text-transparent bg-clip-text bg-gradient-to-r from-[#EB48D5] to-[#D2AE2F]">
-            Leading the Future of {' '} <br />
-            <span className="block">Tech through Education</span>
-          </h1>
-        </div>
-      </div>
+  {/* Navigation Bar */}
+<div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+  <div className="text-center pt-16 pb-12 md:pb-20">
+    <h1 className="font-extrabold tracking-tight text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl text-transparent bg-clip-text bg-gradient-to-r from-[#EB48D5] to-[#D2AE2F] leading-tight">
+      Leading the Future of {' '} <br />
+      <span className="block">Tech through Education</span>
+    </h1>
+  </div>
+</div>
 
       <div className="relative z-10 flex flex-col lg:flex-row justify-center items-center md:mt-8 pb-8 md:pb-16 2xl:pb-24 px-12 xl:px-20">
         <div className="relative w-[350px] md:w-[600px] lg:w-[890px] h-[280px] md:h-[350px] xl:h-[500px] overflow-hidden rounded-xl shadow-lg">
